be36ebe10495c3c211a82f9e34b2f4b5850b2827
Sébastien Tomasi <megapastore@hotmail.com>
2018-12-18 14:32:48 +0100

    adapt the code to use the jamf patch server

--- PostProcessors/PatchManagement.py
+++ PostProcessors/PatchManagement.py
@@ -15,7 +15,7 @@
 # limitations under the License.
 
 from autopkglib import Processor, ProcessorError
-from collections import OrderedDict
+
 import json
 import requests
 import os
@@ -54,28 +54,25 @@ class PatchManagement(Processor):
     def jamf_requests(self, url, action, app_type, payload=None):
         b64Val = base64.b64encode("{0}:{1}".format(self.env["API_USERNAME"], self.env["API_PASSWORD"]))
         authorization = "Basic {0}".format(b64Val)
-        # headers = {
-        #     'Authorization': authorization,
-        #     'Accept': "application/{0}".format(app_type),
-        # }
         if action == "POST":
             response = requests.request("GET", url, data="", headers={"Accept": "application/json", 'Authorization': authorization})
             resp_json = json.loads(response.text)
             if response.status_code == 200:
                 action = "PUT"
             if "softwaretitleconfig" in url:
+                if len(resp_json['patch policies']) == 0:
+                    action = "POST"
                 for entry in resp_json["patch policies"]:
                     patch_policy_name = "{0} - {1}".format(self.env.get("jss_importer_summary_result")["data"]["Name"],self.env.get("jss_importer_summary_result")["data"]["Version"])
                     if entry["name"] == patch_policy_name:
                         entry["id"]
                         url = "{0}/JSSResource/patchpolicies/id/{1}".format(self.env["JSS_URL"], entry["id"])
-
         response = requests.request(action, url, data=payload, headers={"Accept": "application/{0}".format(app_type), 'Authorization': authorization})
-
-        # print action
-        # print url
-        # print headers['Accept']
-        return response
+        print "***********************"
+        print action, url
+        print "status code:", response.status_code
+        print "***********************"
+        return response, action
 
     def find_file_in_override_postprocessors_path(self, path):
         override_path = ""
@@ -83,11 +80,9 @@ class PatchManagement(Processor):
         for entry in self.env["RECIPE_SEARCH_DIRS"]:
             if os.path.exists(os.path.join(entry,"PostProcessors",path)):
                 repo_path = os.path.join(entry,"PostProcessors",path)
-                print repo_path
         for entry in self.env["RECIPE_OVERRIDE_DIRS"]:
             if os.path.exists(os.path.join(os.path.expanduser(entry),path)):
                 override_path = os.path.join(os.path.expanduser(entry),path)
-                print override_path
         if override_path:
             return override_path
         else:
@@ -112,7 +107,8 @@ class PatchManagement(Processor):
     def get_obj_id(self,obj_type,obj_name):
         url = "{0}/JSSResource/{1}/name/{2}".format(self.env["JSS_URL"],obj_type,obj_name)
         # print url
-        obj_data = json.loads(self.jamf_requests(url, "GET", "json").text)
+        response, action = self.jamf_requests(url, "GET", "json")
+        obj_data = json.loads(response.text)
         if obj_type is "packages":
             obj_id = obj_data["package"]["id"]
         elif obj_type is "categories":
@@ -120,53 +116,53 @@ class PatchManagement(Processor):
         return obj_id
 
     def update_softwaretitle(self,replace_dict, jamf_id):
-        # print self.find_file_in_search_path("SoftwareTitle.xml")
         with open(self.find_file_in_override_postprocessors_path("SoftwareTitle.xml"), "r") as template_file:
             text = template_file.read()
         template = self.replace_text(text, replace_dict)
-        # print template
         url = "{0}/JSSResource/patchsoftwaretitles/id/{1}".format(self.env["JSS_URL"],str(jamf_id))
-        response = self.jamf_requests(url, "POST", "xml", template)
-        print "Status: ", response.status_code
-        return response.status_code
+        response, action = self.jamf_requests(url, "POST", "xml", template)
+        return response.status_code, action
 
     def create_patchpolicy(self, replace_dict):
         with open(self.find_file_in_override_postprocessors_path("PatchPolicy.xml"), "r") as template_file:
             text = template_file.read()
         template = self.replace_text(text, replace_dict)
-        # print template
         url = "{0}/JSSResource/patchpolicies/softwaretitleconfig/id/{1}".format(self.env["JSS_URL"],str(replace_dict["JAMF_ID"]))
-        response = self.jamf_requests(url, "POST", "xml",template)
-        print "Status: ", response.status_code
-        return response.status_code
+        response, action = self.jamf_requests(url, "POST", "xml",template)
+        return response.status_code, action
 
     def main(self):
         jss_importer_summary_result = self.env.get("jss_importer_summary_result")
         patch_server_name = self.env.get("patch_server")
-        # print jss_importer_summary_result["data"]["Version"]
-        # print jss_importer_summary_result["data"]["Package"]
-        # print jss_importer_summary_result["data"]["Name"]
-        # print jss_importer_summary_result["data"]["Categories"]
-
-        for server in range(1,11,1):
-            url = "{0}/JSSResource/patchexternalsources/id/{1}".format(self.env["JSS_URL"],server)
-            response = self.jamf_requests(url, "GET", "json")
+        if patch_server_name == "Jamf":
+            status = True
+            url = "{0}/JSSResource/patchinternalsources/id/1".format(self.env["JSS_URL"])
+            response, action = self.jamf_requests(url, "GET", "json")
             if response.status_code == 200:
-                response = json.loads(response.text)["patch_external_source"]
+                response = json.loads(response.text)["patch_internal_source"]
                 if response["name"] == patch_server_name:
                     patch_server = response
-        status = "False"
+        else:
+            for server in range(1,11,1):
+                url = "{0}/JSSResource/patchexternalsources/id/{1}".format(self.env["JSS_URL"],server)
+                response, action = self.jamf_requests(url, "GET", "json")
+                if response.status_code == 200:
+                    response = json.loads(response.text)["patch_external_source"]
+                    if response["name"] == patch_server_name:
+                        patch_server = response
+
         name = jss_importer_summary_result["data"]["Name"]
         pkg_name = jss_importer_summary_result["data"]["Package"]
         name_id = self.env.get("name_id")
         jamf_id = self.env.get("softwaretitleconfig_id")
         version = jss_importer_summary_result["data"]["Version"]
-        url = "https://{0}/patch/{1}".format(patch_server["host_name"], name_id)
-        response = requests.request("GET", url)
-        soft_def = json.loads(response.text)
-        for patch in soft_def["patches"]:
-            if patch["version"] == version:
-                status = True
+        if patch_server_name != "Jamf":
+            url = "https://{0}/patch/{1}".format(patch_server["host_name"], name_id)
+            response = requests.request("GET", url)
+            soft_def = json.loads(response.text)
+            for patch in soft_def["patches"]:
+                if patch["version"] == version:
+                    status = True
         dict_software_title = {
             "NAME": name,
             "NAME_ID": name_id,
@@ -181,11 +177,12 @@ class PatchManagement(Processor):
             "VERSION": version,
         }
         if status:
-                print "Build software title"
-                softwaretitle = self.update_softwaretitle(dict_software_title, jamf_id)
+                softwaretitle, action = self.update_softwaretitle(dict_software_title, jamf_id)
                 if softwaretitle == 201:
-                    print "Patch policy to be created"
-                    self.create_patchpolicy(dict_patch_policy)
+                    print "The software Title '{}' has been {}".format(name, action)
+                    patchpolicy, action = self.create_patchpolicy(dict_patch_policy)
+                    if patchpolicy == 201:
+                        print "The patch policy '{} - {}' has been {}".format(name, version, action)
 
 
 if __name__ == "__main__":
